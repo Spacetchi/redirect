6c6a61ac5831585231b6fae42fde3c8ec35fda8f
Medusa Moxie <Spacetchi@users.noreply.github.com>
2016-12-02 16:27:30 -0800

    update

--- redirect.js
+++ redirect.js
@@ -1,1 +1,9 @@
-window.location = "http://spacetchi.net";
+var current = window.location;
+var current_protocol = current.protocol;
+var current_host = current.hostname;
+var current_pathname = current.pathname;
+var current_hash = current.hash;
+
+var link = current_protocol + current_host + current_pathname + current_hash;
+
+window.location = link;
